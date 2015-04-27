8ef93d6b07bd125663f799443f4435de644d9992
Jeanette Fairless & Jocelyn Alsdorf <student@epicodus.com>
2015-04-27 11:26:20 -0700

    it creates instance of a MyHash class, it stores key value pairs and fetches the value when key is iinput

--- lib/diy_hash.rb
+++ lib/diy_hash.rb
@@ -1,9 +1,16 @@
 class MyHash
-  define_method(:initialize) do |key, val|
-    @key = key
-    @val = val
+  define_method(:initialize) do
     new_hash = self
-    new_hash = {@key => @val}
-    puts new_hash
+    @new_hash = {}
+    puts @new_hash
   end
+
+  define_method(:store) do |key, val|
+  add_hash = {key => val}
+  @new_hash.merge!(add_hash)
+  end
+
+define_method(:fetch) do |key|
+  @new_hash[key]
+end
 end
